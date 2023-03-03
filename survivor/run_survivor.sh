c8a59c39f8dc269cd01bb7a0e60bbc42ba687a94
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-03-03 16:08:44 +0100

    SURVIVOR analysis and SV venn diagram generation scripts

---
+++ survivor/run_survivor.sh
@@ -0,0 +1,70 @@
+# SURVIVOR Version: 1.0.7
+# https://github.com/fritzsedlazeck/SURVIVOR
+
+# Expected binary file in "bin"
+SURVIVOR="bin/SURVIVOR"
+
+# Expected input in "data" directory:
+# - illumina.tiddit.vcf
+# - ont.sniffles.vcf
+# - pb.sniffles.vcf
+
+MAX_DISTANCE=1000
+MIN_BP=100
+SMALL=1000
+MEDIUM=10000
+LARGE=1000000
+
+OUT_DIR="out"
+DATA_DIR="data"
+BLACKLIST="ref/hg38-blacklist.v2.bed"
+
+VCF_LIST="$OUT_DIR/vcfs"
+echo "$DATA_DIR/illumina.tiddit.vcf" >> $VCF_LIST
+echo "$DATA_DIR/ont.sniffles.vcf" >> $VCF_LIST
+echo "$DATA_DIR/pb.sniffles.vcf" >> $VCF_LIST
+
+### MERGE ALL SVS ###
+$SURVIVOR merge $VCF_LIST $MAX_DISTANCE 1 1 1 0 $MIN_BP "$OUT_DIR/REH_merged.tmp.vcf"
+$SURVIVOR filter "$OUT_DIR/REH_merged.tmp.vcf" $BLACKLIST $MIN_BP -1 0 -1 "$OUT_DIR/REH_merged_all.vcf"
+
+### CONSENSUS SETS ###
+$SURVIVOR merge $VCF_LIST $MAX_DISTANCE 2 1 1 0 $MIN_BP "$OUT_DIR/REH_consensus_2x.vcf" 
+$SURVIVOR merge $VCF_LIST $MAX_DISTANCE 3 1 1 0 $MIN_BP "$OUT_DIR/REH_consensus_3x.vcf"
+
+### NON-TRANSLOCATION STATISTICS ###
+# Strip out BNDs
+grep -v "SVTYPE=BND" "$DATA_DIR/illumina.tiddit.vcf" > "$OUT_DIR/illumina.nobnd.vcf"
+grep -v "SVTYPE=BND" "$DATA_DIR/ont.sniffles.vcf" > "$OUT_DIR/ont.nobnd.vcf"
+grep -v "SVTYPE=BND" "$DATA_DIR/pb.sniffles.vcf" > "$OUT_DIR/pb.nobnd.vcf"
+
+echo "$OUT_DIR/illumina.nobnd.vcf" > $VCF_LIST
+echo "$OUT_DIR/ont.nobnd.vcf" >> $VCF_LIST
+echo "$OUT_DIR/pb.nobnd.vcf" >> $VCF_LIST
+
+# Merge Non-translocations only
+$SURVIVOR merge $VCF_LIST $MAX_DISTANCE 1 1 1 0 $MIN_BP "$OUT_DIR/REH_merged_nobnd.tmp.vcf"
+$SURVIVOR filter "$OUT_DIR/REH_merged_nobnd.tmp.vcf" $BLACKLIST $MIN_BP -1 0 -1 "$OUT_DIR/REH_merged_nobnd.vcf"
+
+# Merged non-translocations by size
+$SURVIVOR filter "$OUT_DIR/REH_merged_nobnd.vcf" $BLACKLIST $MIN_BP $SMALL 0 -1 "$OUT_DIR/REH_merged_small.vcf"
+$SURVIVOR filter "$OUT_DIR/REH_merged_nobnd.vcf" $BLACKLIST $SMALL $MEDIUM  0 -1 "$OUT_DIR/REH_merged_medium.vcf"
+$SURVIVOR filter "$OUT_DIR/REH_merged_nobnd.vcf" $BLACKLIST $MEDIUM -1 0 -1 "$OUT_DIR/REH_merged_large.vcf"
+
+### MATRICES FOR VENN DIAGRAMS ###
+NORMALIZE=0
+# All SVs
+$SURVIVOR genComp "$OUT_DIR/REH_merged_all.vcf" $NORMALIZE "$OUT_DIR/REH_merged_all.mat.txt"
+perl -ne 'print "$1\n" if /SUPP_VEC=([^,;]+)/' "$OUT_DIR/REH_merged_all.vcf" | sed -e 's/\(.\)/\1 /g' > "$OUT_DIR/REH_merged_all_overlap.txt"
+
+# Small
+$SURVIVOR genComp "$OUT_DIR/REH_merged_small.vcf" $NORMALIZE "$OUT_DIR/REH_merged_small.mat.txt"
+perl -ne 'print "$1\n" if /SUPP_VEC=([^,;]+)/' "$OUT_DIR/REH_merged_small.vcf" | sed -e 's/\(.\)/\1 /g' > "$OUT_DIR/REH_merged_small_overlap.txt"
+
+# Medium
+$SURVIVOR genComp "$OUT_DIR/REH_merged_medium.vcf" $NORMALIZE "$OUT_DIR/REH_merged_medium.mat.txt"
+perl -ne 'print "$1\n" if /SUPP_VEC=([^,;]+)/' "$OUT_DIR/REH_merged_medium.vcf" | sed -e 's/\(.\)/\1 /g' > "$OUT_DIR/REH_merged_medium_overlap.txt"
+
+# Large
+$SURVIVOR genComp "$OUT_DIR/REH_merged_large.vcf" $NORMALIZE  "$OUT_DIR/REH_merged_large.mat.txt"
+perl -ne 'print "$1\n" if /SUPP_VEC=([^,;]+)/' "$OUT_DIR/REH_merged_large.vcf" | sed -e 's/\(.\)/\1 /g' > "$OUT_DIR/REH_merged_large_overlap.txt"
---
+++ venn/run_venn.r
@@ -0,0 +1,61 @@
+# Expected files in "../survivor/out" (output from run_survivor.sh):
+# - REH_merged_all_overlap.txt
+# - REH_merged_small_overlap.txt
+# - REH_merged_medium_overlap.txt
+# - REH_merged_large_overlap.txt
+
+library(VennDiagram)
+library(RColorBrewer)
+
+colors <- brewer.pal(3, "Pastel2")
+
+a_val=0.5
+alpha=rep(a_val, 3)
+
+fontfamily="sans"
+fontface="bold"
+
+text_size_numbers = .5
+text_size_labels = 0
+
+draw_venn <- function(overlap_file, output_file) {
+  input = paste("../survivor/out/", overlap_file, sep = "")
+  output = paste("out/", output_file, sep = "")
+  t=read.table(input,header=F)
+  venn_data=list(Illumina=which(t[,1]==1), ONT=which(t[,2]==1), PB=which(t[,3]==1))
+  venn.diagram(venn_data,  
+               fill = colors, 
+               alpha = alpha, 
+               filename = output, 
+               output=TRUE,
+               
+               # Output features
+               imagetype="png" ,
+               height = 480 , 
+               width = 480 , 
+               resolution = 300,
+               compression = "lzw",
+               
+               # Circles
+               lwd = 2,
+               lty = 'blank',
+               
+               # Numbers
+               cex = text_size_numbers,
+               fontface = fontface,
+               fontfamily = fontfamily,
+               
+               # Set names
+               cat.cex = text_size_labels,
+               cat.fontface = fontface,
+               cat.default.pos = "outer",
+               cat.pos = c(-27, 27, 135),
+               cat.dist = c(0.055, 0.055, 0.085),
+               cat.fontfamily = fontfamily,
+               rotation = 1);
+}
+
+draw_venn("REH_merged_all_overlap.txt", "venn_all.png")
+draw_venn("REH_merged_small_overlap.txt", "venn_small.png")
+draw_venn("REH_merged_medium_overlap.txt", "venn_medium.png")
+draw_venn("REH_merged_large_overlap.txt", "venn_large.png")
