afa910e84d881a3e58d7e436626dd9526775b9ed
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-03-04 10:20:42 +0100

    Stripplot generation scripts

---
+++ stripplot/01_survivor.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Expected input in "../data" directory:
+# - illumina.tiddit.vcf
+# - ont.sniffles.vcf
+# - pb.sniffles.vcf
+
+# PATHS TO BINARIES
+# Expected binary file in "../survivor/bin":
+# SURVIVOR Version: 1.0.7
+# https://github.com/fritzsedlazeck/SURVIVOR
+SURVIVOR="../survivor/bin/SURVIVOR"
+
+# PATHS
+OUT_DIR="out"
+DATA_DIR="../data"
+TMP_DIR="/tmp"
+
+# SETTINGS
+MAX_DISTANCE=1000
+MIN_BP=100
+
+### GENERATE CONSENSUS SET ###
+VCF_LIST="$OUT_DIR/vcfs"
+echo "$DATA_DIR/illumina.tiddit.vcf" >> $VCF_LIST
+echo "$DATA_DIR/ont.sniffles.vcf" >> $VCF_LIST
+echo "$DATA_DIR/pb.sniffles.vcf" >> $VCF_LIST
+
+$SURVIVOR merge $VCF_LIST $MAX_DISTANCE 3 1 1 0 $MIN_BP "$OUT_DIR/REH_consensus_3x.vcf"
---
+++ stripplot/02_stripplot.py
@@ -0,0 +1,127 @@
+
+import pandas
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pysam import VariantFile
+import pickle
+
+# Input files
+DATA_DIR = "../data"
+ILLUMINA = DATA_DIR + "/illumina.tiddit.vcf"
+PB = DATA_DIR + "/pb.sniffles.vcf"
+ONT = DATA_DIR + "/ont.sniffles.vcf"
+CONSENSUS = "out/REH_consensus_3x.vcf"
+
+# Output files
+OUT_DIR="out"
+SIZES_DB=OUT_DIR + "/sizes.pickle"
+OUT_FILE=OUT_DIR + "/stripplot.png"
+
+MIN_BP = 100
+
+do_process_sizes = False
+try:
+    with open(SIZES_DB, 'rb') as f:
+        sizes_db = pickle.load(f)
+except:
+    do_process_sizes = True
+    sizes_db = {}
+
+def process_sizes():
+	illumina = VariantFile(ILLUMINA)
+	pb = VariantFile(PB)
+	ont = VariantFile(ONT)
+	consensus = VariantFile(CONSENSUS)
+
+	# PREPARE NEW DATA FRAME
+	columns = [
+		"dataset",
+		"size"
+	]
+	sizes = pandas.DataFrame(columns=columns)
+
+	# HELPER FUNCTIOMS
+	def sv_length(rec):
+		len = rec.info.get('SVLEN')
+		try:
+			return abs(len[0])
+		except:
+			try: 
+				return abs(len)
+			except:
+				return 0
+
+
+	def append_size(size, dataset, sizes):
+		if (size < MIN_BP):
+			return sizes
+
+		row = {
+			"dataset": dataset,
+			"size": size,
+		}
+		df = pandas.DataFrame([row])
+		return pandas.concat([sizes, df], ignore_index=True)
+
+
+	for rec in illumina.fetch():
+		sizes = append_size(sv_length(rec), "ILLUMINA", sizes)
+
+
+	for rec in pb.fetch():
+		sizes = append_size(sv_length(rec), "PB", sizes)
+
+
+	for rec in ont.fetch():
+		sizes = append_size(sv_length(rec), "ONT", sizes)
+
+	for rec in consensus.fetch():
+		sizes = append_size(sv_length(rec), "CONSENSUS", sizes)
+
+	sizes = pandas.DataFrame(sizes.to_dict())
+	with open(SIZES_DB, 'wb') as f:
+		pickle.dump(sizes, f)
+	
+	return sizes
+
+if do_process_sizes:
+	sizes_db = process_sizes()
+
+# Size plots
+SMALL=1000
+MEDIUM=10000
+LARGE=1000000
+
+plt.figure(figsize=(16, 11))
+plt.subplot(4, 1, 1)
+plt.xlim(MIN_BP,SMALL)
+a=0.4
+ax = sns.stripplot(x='size', y='dataset', data=sizes_db, color='#a73c52',
+              alpha=a, size=4)
+ax.set(xlabel='size (bp)', ylabel='')
+ax.set_title(label="Small SVs (100 bp - 1 kbp)", loc='right', fontweight="bold")
+
+plt.subplot(4, 1, 2)
+ax = sns.stripplot(x='size', y='dataset', data=sizes_db, color='#6b5f88',
+              alpha=a, size=4)
+plt.xlim(SMALL,MEDIUM)
+ax.set(xlabel='size (bp)', ylabel='')
+ax.set_title(label="Medium SVs (1 kbp - 10 kbp)", loc='right', fontweight="bold")
+
+
+plt.subplot(4, 1, 3)
+ax = sns.stripplot(x='size', y='dataset', data=sizes_db, color='#3780b3',
+              alpha=a, size=4)
+plt.xlim(MEDIUM,LARGE)
+ax.set(xlabel='size (Mbp)', ylabel='')
+ax.set_title(label="Large SVs (10 kbp - 1 Mbp)", loc='right', fontweight="bold")
+
+plt.subplot(4, 1, 4)
+ax = sns.stripplot(x='size', y='dataset', data=sizes_db, color='#47a266',
+              alpha=a, size=4)
+plt.xlim(MIN_BP,8000000)
+ax.set(xlabel='size (Mbp)', ylabel='')
+ax.set_title(label="All SVs (100 bp - 8 Mbp)", loc='right', fontweight="bold")
+
+plt.tight_layout()
+plt.savefig(OUT_FILE)
